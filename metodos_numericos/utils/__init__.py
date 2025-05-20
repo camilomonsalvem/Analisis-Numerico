246242d1b309d8a52b193edecb916db74c088c86
Emmanuel Castañeda Cano <ecastanedc@eafit.edu.co>
2025-05-20 16:15:55 -0500

    Metodos y creación de carpetas

---
+++ metodos_numericos/utils/__init__.py
@@ -0,0 +1,23 @@
+from .Biseccion import biseccion
+from .Gauss import gauss
+from .Grafico import grafico
+from .Jacobi import jacobi
+from .Newton import newton
+from .PuntoFijo import punto_fijo
+from .RaicesMultiples import raices_multiples
+from .ReglaFalsa import regla_falsa
+from .Secante import secante
+from .Sor import sor
+
+__all__ = [
+    "biseccion",
+    "gauss",
+    "grafico",
+    "jacobi",
+    "newton",
+    "punto_fijo",
+    "raices_multiples",
+    "regla_falsa",
+    "secante",
+    "sor",
+]
